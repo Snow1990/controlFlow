9cec4c0768565054fea9d15f60a1d319d18a8d62
SN <snn@shaonandeMacBook-Pro.local>
2014-11-19 10:25:56 +0800

    Initial Commit

---
+++ controlFlow/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  controlFlow
+//
+//  Created by SN on 14/11/19.
+//  Copyright (c) 2014年 maiziedu. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
